2fb379c47e964478dd8d59fbb0b49fe24e03c2e7
DeliDoktoru <yunusemrahdursun@gmail.com>
2023-11-19 13:29:48 +0300

    update

--- src/public/scripts/pages/invite.js
+++ src/public/scripts/pages/invite.js
@@ -4,10 +4,36 @@ function generateLink(){
     $("#createdLink").val(data.d)
 
   }
+  async function copyToClipboard(textToCopy) {
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+            
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+            
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    }
+}
 function copyIt(){
     let copyText = document.querySelector('#createdLink')
-    copyText.select();
-    navigator.clipboard.writeText(copyText.value);
+    // copyText.select();
+    // navigator.clipboard.writeText(copyText.value);
+    copyToClipboard(copyText.value)
 }
 
 $(function() {
